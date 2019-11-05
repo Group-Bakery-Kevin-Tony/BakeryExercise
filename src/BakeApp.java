f183f46bf47b5d99ca3e7f636d89248b957f5d57
kcheung3102 <kcheung3102@gmail.com>
2019-11-05 13:06:22 -0500

    commit

--- src/BakeApp.java
+++ src/BakeApp.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class BakeApp {
 
     public static void main(String[] args) {
-        Scanner input =   new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Welcome to Brookley's Better Bakery App!");
         System.out.println("We contain peanut, gluten, soy and dairy in our products");
 
         System.out.println("To search for foods that you can eat, please enter the name of the diet restriction: ");
         String userInput = input.nextLine().trim().toLowerCase();
-<<<<<<< HEAD
-        BakeGoodsDB.checkRestriction(userInput);
-=======
+        BakeGoodsDB.displayData(userInput);
+
 
         ArrayList<BakedGoodsData> bakedGoods = new ArrayList<>();
 
         BakedGoodsData bananaBread = new BakedGoodsData("Better Banana Bread", 12.99, "11/11/19", 4, "gluten");
         BakedGoodsData cinnamonBread = new BakedGoodsData("Cinnamon Bread", 9.99, "11/2/19", 7, "dairy");
-        BakedGoodsData wheatBread = new BakedGoodsData("Neat Wheat", 4.99, "11/2/19",5, "gluten");
+        BakedGoodsData wheatBread = new BakedGoodsData("Neat Wheat", 4.99, "11/2/19", 5, "gluten");
         BakedGoodsData whiteBread = new BakedGoodsData("Wonderful Bread", 2.99, "11/1/19", 24, "gluten");
         BakedGoodsData peanutButterBread = new BakedGoodsData("Jiffy Bread", 5.99, "11/2/19", 3, "peanut");
         BakedGoodsData raisinBread = new BakedGoodsData("Crazin' Raisin", 12.99, "11/7/19", 5, "dairy");
@@ -35,23 +34,8 @@ public class BakeApp {
         BakedGoodsData pastry = new BakedGoodsData("The Great Danish", 9.99, "11/2/19", 5, "gluten");
         BakedGoodsData pieCrust = new BakedGoodsData("Humble Pie", 12.99, "11/2/19", 7, "soy");
 
-        Collections.addAll (bakedGoods, bananaBread, cinnamonBread, wheatBread, whiteBread, peanutButterBread, raisinBread, soyMuffins, cinnamonRolls, savoryRolls, blueberryMuffins, pizzaCrust, frenchBread, cake, donut, pastry, pieCrust);
+        Collections.addAll(bakedGoods, bananaBread, cinnamonBread, wheatBread, whiteBread, peanutButterBread, raisinBread, soyMuffins, cinnamonRolls, savoryRolls, blueberryMuffins, pizzaCrust, frenchBread, cake, donut, pastry, pieCrust);
 
-        switch(userInput){
-            case "gluten":
-                //call objects from bakedGoodsDB that do not contain this
-            case "peanut":
-                //call objects from bakedGoodsDB that do not contain this
-            case "soy":
-                //call objects from bakedGoodsDB that do not contain this
-            case "dairy":
-                //call objects from bakedGoodsDB that do not contain this
-            case "all":
-                //calls all objects.
-            default:
-                System.out.println("invalid diet restriction");
 
-        }
->>>>>>> de09996fdb498494dbf7cee8e9ab10b05b798018
     }
 }
--- src/BakeGoodsDB.java
+++ src/BakeGoodsDB.java
@@ -1,28 +1,45 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class BakeGoodsDB {
+    private ArrayList<BakedGoodsData> bakedGoods;
+
+    private BakeGoodsDB(){};
+
+    public ArrayList<BakedGoodsData> getBakedGoods() {
+        return bakedGoods;
+    }
+
+    public void setBakedGoods(ArrayList<BakedGoodsData> bakedGoods) {
+        this.bakedGoods = bakedGoods;
+    }
+
 
 
 
     //enhanced For Loop that calls the different objects that dont contain this restriction.
-    //For(
 
-    public static void checkRestriction(String userInput) {
-        switch (userInput){
-            case "gluten":
 
-            case "peanut":
-                //call objects from bakedGoodsDB that do not contain this
-            case "soy":
-                //call objects from bakedGoodsDB that do not contain this
-            case "dairy":
-                //call objects from bakedGoodsDB that do not contain this
-            case "all":
-                //calls all objects.
-            default:
-                System.out.println("invalid diet restriction");
 
+
+    public static void displayData(String userInput) {
+        for( Object bakedItems: bakedGoods ) {
+            switch (userInput) {
+                case "gluten":
+                        //call objects from arrayList
+                case "peanut":
+                    //call objects from bakedGoodsDB that do not contain this
+                case "soy":
+                    //call objects from bakedGoodsDB that do not contain this
+                case "dairy":
+                    //call objects from bakedGoodsDB that do not contain this
+                case "all":
+                    //calls all objects.
+                default:
+                    System.out.println("invalid diet restriction");
+
+            }
         }
 }
 
