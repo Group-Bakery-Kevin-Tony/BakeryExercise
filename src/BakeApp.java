f848598153ec62a8dbbfc57ab197dc8f451a183e
kcheung3102 <kcheung3102@gmail.com>
2019-11-05 10:30:42 -0500

    BakeApp

--- src/BakeApp.java
+++ src/BakeApp.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class BakeApp {
 
     public static void main(String[] args) {
+        Scanner input =   new Scanner(System.in);
 
+        System.out.println("Welcome to Brookley's Better Bakery App!");
+        System.out.println("We contain peanut, glueten, soy and dairy in our products");
+
+        System.out.println("To search for foods that you can eat, please enter the name of the diet restriction: ");
+        String userInput = input.nextLine().trim().toLowerCase();
+
+        switch(userInput){
+            case "gluten":
+                //call objects from bakedGoodsDB that do not contain this
+            case "peanut":
+                //call objects from bakedGoodsDB that do not contain this
+            case "soy":
+                //call objects from bakedGoodsDB that do not contain this
+            case "dairy":
+                //call objects from bakedGoodsDB that do not contain this
+            case "all":
+                //calls all objects.
+            default:
+                System.out.println("invalid diet restriction");
+
+        }
     }
 }
--- src/BakeGoodsDB.java
+++ src/BakeGoodsDB.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+
 public class BakeGoodsDB {
+    ArrayList<BakedGoodsData> bakedGoods = new ArrayList<>();
+
+    //enhanced For Loop that calls the different objects that dont contain this restriction.
+
 }
